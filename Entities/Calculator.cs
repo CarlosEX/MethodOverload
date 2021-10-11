38f4cb08ec9cc4953a326f768e6383e0ad81f8df
Carlos Antonio <carlos22martinssilva@gmail.com>
2021-10-11 17:24:57 -0300

    comit initial

---
+++ Entities/Calculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MethodOverload.Entities {
+    public class Calculator {
+        public static int Sum(int a, int b) => a + b;
+        public static double Sum(double a, double b) => a + b;
+        public static double Sum(double a) => a;
+        public static float Sum(float a, float b) => a + b;
+        public static object[] Sum(object[] obj) => obj;
+        public static decimal Sum(decimal a) => a + 5;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MethodOverload.Entities;
+
+namespace MethodOverload {
+    class Program {
+        static void Main(string[] args) {
+            var result = Calculator.Sum(5,8);
+            Console.WriteLine(result);
+        }
+    }
+}
